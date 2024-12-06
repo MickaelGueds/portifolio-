b2cf081969fb2c7c42f49593e2d5545d97f97401
MicksCoder <mickaelgoliveira@gmail.com>
2024-12-06 15:22:38 -0300

    testando subir

--- meu-projeto/src/components/feed/index.tsx
+++ meu-projeto/src/components/feed/index.tsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react';
 export function InstaFeed() {
     async function getInstaFeed() {
         const token = import.meta.env.VITE_INSTA_TOKEN;
-        const fields = "media_url,media_type,permalink";
         const url = `https://graph.instagram.com/me?fields=id,username&access_token=${token}`;
         
 
