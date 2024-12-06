3c83fe4689de2655786feca480dae54baefd42ef
MicksCoder <mickaelgoliveira@gmail.com>
2024-12-06 15:13:24 -0300

    teste

--- meu-projeto/src/components/feed/index.tsx
+++ meu-projeto/src/components/feed/index.tsx
@@ -5,7 +5,8 @@ export function InstaFeed() {
     async function getInstaFeed() {
         const token = import.meta.env.VITE_INSTA_TOKEN;
         const fields = "media_url,media_type,permalink";
-        const url = ` https://graph.instagram.com/me?fields=id,username&access_token=${token}&fields=${fields}`;
+        const url = `https://graph.instagram.com/me?fields=id,username&access_token=${token}`;
+        
 
         const {data} = await axios.get(url);
         console.log(data)
